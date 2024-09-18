21e203d5387207c5223a3b3db8e05fbd684e13d3
Jerome Zhao <147390861+TerukiIIM@users.noreply.github.com>
2024-09-18 18:08:50 +0200

    Update API

--- BACK/index.js
+++ BACK/index.js
@@ -1,35 +1,31 @@
-const key = "API_KEY"; // Mettre l'API key
+const apiKey = "API_KEY" // Mettre l'API key
 
-async function getCurrencies() {
-  const response = await fetch(
-    `https://v6.exchangerate-api.com/v6/${key}/codes`
-  );
-
-  return response.json();
+const getCurrencies = async () => {
+    return fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/codes`).then(r => r.json())
 }
 
-async function loadCurrencies() {
-  const res = await getCurrencies();
-  const currenciesList = res.supported_codes;
-
-  return currenciesList;
+const loadCurrencies = async () => {
+    const res = await getCurrencies()
+    const currenciesList = res.supported_codes
+    return currenciesList
 }
 
-function createFormValues(value) {
-  console.log(value);
-  return `<option value="${value}">${value[0]} | ${value[1]}</option>`;
+const createFormValues = (value) => {
+    return `<option value="${value[0]}">${value[0]} | ${value[1]}</option>`
 }
 
-let currencies = loadCurrencies();
+let currencies = loadCurrencies()
 
-const toCurrencyList = document.getElementById("to-currency");
-const fromCurrencyList = document.getElementById("from-currency");
+const fromCurrencyList = document.getElementById("from_currency")
+const toCurrencyList = document.getElementById("to_currency")
 
 currencies.then((currenciesList) => {
-  console.log(currenciesList);
-  currenciesList.forEach((currency) => {
-    // console.log(currency);
-    toCurrencyList.innerHTML += createFormValues(currency);
-    fromCurrencyList.innerHTML += createFormValues(currency);
-  });
-});
+    currenciesList.forEach((currency) => {
+        fromCurrencyList.innerHTML += createFormValues(currency)
+        toCurrencyList.innerHTML += createFormValues(currency)
+    })
+
+    // Initialisation par défaut
+    fromCurrencyList.value = "EUR"
+    toCurrencyList.value = "USD"
+})
